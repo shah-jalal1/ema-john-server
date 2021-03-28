f971b447708f7a9980a2bffc87bc15dfab47ebed
shahjalalcse <shah-jalal1>
2021-03-28 23:04:49 +0600

    database connected, environment setup

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.owenr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const products = client.db("emaJohnStore").collection("products");
+    console.log("database connected");
+
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        products.insertOne(product)
+        .then(result => {
+            console.log(result);
+        })
+    })
+
+});
+
+
+app.listen(port)
