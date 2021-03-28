d7fca26974942eea7074d5149a02fa00d7203f16
shahjalalcse <shah-jalal1>
2021-03-29 00:51:33 +0600

    api added

--- index.js
+++ index.js
@@ -21,11 +21,33 @@ client.connect(err => {
     app.post('/addProduct', (req, res) => {
         const products = req.body;
         // console.log(products);
-        productsCollection.insertMany(products)
+        productsCollection.insertOne(products)
         .then(result => {
             // console.log(result);
             console.log(result.insertedCount);
             res.send(result.insertedCount)
+        })
+    })
+
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents)
+        })
+    })
+
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({key: req.params.key})
+        .toArray( (err, documents) => {
+            res.send(documents[0]);
+        })
+    })
+
+    app.post('/productsByKeys', (req, res) => {
+        const productKeys = req.body;
+        productsCollection.find({key: { $in: productKeys} })
+        .toArray( (err, documents) => {
+            res.send(documents);
         })
     })
 
