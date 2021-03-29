62acb456d3f461e9358279f784fd2b931320513b
shahjalalcse <shah-jalal1>
2021-03-29 11:32:41 +0600

    done

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ const port = 5000;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const productsCollection = client.db("emaJohnStore").collection("products");
+    const ordersCollection = client.db("emaJohnStore").collection("orders");
     console.log("database connected");
 
     app.post('/addProduct', (req, res) => {
         const products = req.body;
-        // console.log(products);
         productsCollection.insertOne(products)
         .then(result => {
-            // console.log(result);
             console.log(result.insertedCount);
             res.send(result.insertedCount)
         })
@@ -31,8 +30,8 @@ client.connect(err => {
 
     app.get('/products', (req, res) => {
         productsCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents)
+        .toArray( (err, documents) => {
+            res.send(documents);
         })
     })
 
@@ -51,7 +50,14 @@ client.connect(err => {
         })
     })
 
-});
+    app.post('/addOrder', (req, res) => {
+        const order = req.body;
+        ordersCollection.insertOne(order)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    })
 
+});
 
 app.listen(port)
